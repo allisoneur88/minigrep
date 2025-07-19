72152eccfb375a0da8a70cfecb628b63bd7d9012
allisoneur88 <adganshin@gmail.com>
2025-07-19 15:42:13 +0200

    refactored logic, added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Error parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("Searching for: {:?}", query);
-    println!("In file: {:?}", filename);
+    println!("Searching for: {:?}", config.query);
+    println!("In file: {:?}", config.filename);
 
     let contents =
-        fs::read_to_string(filename).expect("Something went wrong while reading the file");
+        fs::read_to_string(config.filename).expect("Something went wrong while reading the file");
 
-    println!("{contents}");
+    println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
